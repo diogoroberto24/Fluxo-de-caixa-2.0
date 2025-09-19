0a1935b150a74bcc674ad5c4618de7e55ff7d578
Matheus Caldeira <matheuscardozo4@gmail.com>
2025-09-18 22:15:34 -0300

    chore: remocao de arquivos nao usados

--- src/db.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "../lib/generated/prisma/index.js"
-const prisma = new PrismaClient()
-
-async function getUser(){
-    const user = await prisma.client.findMany()
-    console.log(user)
-}
-
-getUser()
